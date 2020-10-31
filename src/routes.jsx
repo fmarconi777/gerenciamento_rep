fd389518d05e6add6fa019bd3428cad9f70ed80f
Felipe <marconizero@gmail.com>
2020-10-31 19:30:47 -0300

    projeto reiniciado

---
+++ src/routes.jsx
@@ -0,0 +1,14 @@
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import Login from './pages/Login';
+
+function Routes() {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" exact component={Login} />
+            </Switch>
+        </BrowserRouter>
+    )
+}
+
+export default Routes;
